bb54ace1a02ff9d91a879451a7929da5acbd8e9a
harriperhana <harri.huovinen@gmail.com>
2025-02-15 14:56:22 +0200

    language support

--- firefox-extension/auto-refresh/content.js
+++ firefox-extension/auto-refresh/content.js
@@ -46,12 +46,72 @@ function findUpdateButton(shadowRoot) {
 
                     console.log("Button detected");
 
-                    if (["Päivitä", "Update"].includes(ha_button.getAttribute("aria-label"))) {
+                    if ([
+                        "Opdateer", // Afrikaans
+                        "تحديث", // العربية
+                        "Обновяване", // Български
+                        "আপডেট", // বাংলা
+                        "Actualitza", // Català
+                        "Aktualizovat", // Čeština
+                        "Opdater", // Dansk
+                        "Aktualisieren", // Deutsch
+                        "Aktualisiere", // Schwyzerdütsch
+                        "Ανανέωση", // Ελληνικά
+                        "Update", // English
+                        "Actualizar", // Español
+                        "Uuenda", // Eesti
+                        "Eguneratu", // Euskara
+                        "به‌روز کنید", // فارسی
+                        "Päivitä", // Suomi
+                        "Mettre à jour", // Français
+                        "Actualizar", // Galician
+                        "અપડેટ", // ગુજરાતી
+                        "עדכון", // עברית
+                        "अपडेट", // हिन्दी
+                        "Ažuriraj", // Hrvatski
+                        "Frissítés", // Magyar
+                        "Perbarui", // Bahasa Indonesia
+                        "Uppfæra", // Íslenska
+                        "Aggiorna", // Italiano
+                        "განახლება", // ქართული
+                        "ನವೀಕರಿಸಿ", // ಕನ್ನಡ
+                        "업데이트", // 한국어
+                        "Aktualiséieren", // Lëtzebuergesch
+                        "Atnaujinti", // Lietuvių
+                        "Atjaunināt", // Latviešu
+                        "അപ്ഡേറ്റ്", // മലയാളം
+                        "Шинэчлэх", // Монгол хэл
+                        "अपडेट", // मराठी
+                        "Kemaskini", // Bahasa Melayu
+                        "Oppdater", // Norsk Bokmål
+                        "अपडेट गर्नुहोस्", // नेपाली
+                        "Bijwerken", // Nederlands
+                        "Aktualizować", // Polski
+                        "Atualizar", // Português
+                        "Atualizar", // Português (Brasil)
+                        "Actualizare", // Română
+                        "Обновить", // Русский
+                        "Aktualizovať", // Slovenčina
+                        "Posodobi", // Slovenščina
+                        "Ажурирај", // Српски
+                        "Ažuriraj", // Srpski
+                        "Uppdatera", // Svenska
+                        "Sasisha", // Kiswahili
+                        "అప్‌డేట్", // తెలుగు
+                        "อัปเดต", // ภาษาไทย
+                        "Güncelle", // Türkçe
+                        "Оновити", // Українська
+                        "اپ ڈیٹ کریں", // اُردُو
+                        "Cập nhật", // Tiếng Việt
+                        "更新", // 简体中文
+                        "更新", // 繁體中文（香港）
+                        "更新" // 繁體中文（台灣）
+                      ].includes(ha_button.getAttribute("aria-label"))) {
 
                         location.reload();
 
                     }
-                    
+
                 }
 
             }, 1000);
